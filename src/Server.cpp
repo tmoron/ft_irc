ad12e01911013684ec83084be4aea6adec3dd00d
Ubuntu Laptop <hulo1920@gmail.com>
2024-07-22 17:15:51 +0200

    fix main

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Server.hpp                                         :+:      :+:    :+:   */
+/*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hubourge <hubourge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 15:55:07 by pageblanche       #+#    #+#             */
-/*   Updated: 2024/07/22 17:11:27 by hubourge         ###   ########.fr       */
+/*   Created: 2024/07/22 16:04:07 by hubourge          #+#    #+#             */
+/*   Updated: 2024/07/22 16:18:36 by hubourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_HPP
-# define SERVER_HPP
+#include "Server.hpp"
 
-#include <iostream>
-
-class Server {
-	private:
-		int			_port;
-		int			_servSocketFd;
-		std::string	_password;
-
-	public:
-		Server(std::string port, std::string password); // Default constructor
-};
-
-#endif
+/*-------------------------------- Constructor -------------------------------*/
+Server::Server(std::string port, std::string password) {
+	
+}
