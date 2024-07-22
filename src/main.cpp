fb46cfe812b81be631b9a4a2d298fa6d5512e8c7
tom moron <tom.moron@icloud.com>
2024-07-22 17:20:26 +0200

    merge main

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,20 @@
 /*   By: hubourge <hubourge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 15:56:17 by pageblanche       #+#    #+#             */
-/*   Updated: 2024/07/22 17:15:36 by hubourge         ###   ########.fr       */
+/*   Updated: 2024/07/22 17:20:01 by tomoron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+int main(int argc, char **argv)
 {
+	if(argc != 3)
+	{
+		std::cerr << "Error : Wrong number of arguments" << std::endl;
+		std::cerr << "	Usage: " << argv[0] << " <port> <password>" << std::endl;
+		return(1);
+	}
+	Server srv(std::string(argv[1]), std::string(argv[2]));
 
+	srv.listen();
     return(252);
 }
