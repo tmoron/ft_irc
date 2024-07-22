671b79de7e7e9db85a57f66d6aef7763f79faa23
PageBlanche <axeldubois@orange.fr>
2024-07-22 17:09:58 +0200

    class client

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,12 +6,25 @@
 /*   By: pageblanche <pageblanche@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 15:53:55 by pageblanche       #+#    #+#             */
-/*   Updated: 2024/07/22 16:00:04 by pageblanche      ###   ########.fr       */
+/*   Updated: 2024/07/22 16:17:05 by pageblanche      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_HPP
 # define CLIENT_HPP
+
+#include <string>
+
+class Client
+{
+    private:
+        int         nb;
+        std::string _name;
+        std::string _buffer;
+        Client(/* args */);
+    public:
+        ~Client();
+};
 
 
 
