90430800a13a4778fd955cfa151f8332ace095e9
Ubuntu Laptop <hulo1920@gmail.com>
2024-07-23 16:54:00 +0200

    norm zebi

--- include/Command.hpp
+++ include/Command.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Command.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pageblanche <pageblanche@student.42.fr>    +#+  +:+       +#+        */
+/*   By: hubourge <hubourge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 14:20:42 by pageblanche       #+#    #+#             */
-/*   Updated: 2024/07/23 15:12:49 by pageblanche      ###   ########.fr       */
+/*   Updated: 2024/07/23 16:52:32 by hubourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@
 
 class Command
 {
-private:
-    std::string _type;
-    void        (*_funct)();
-public:
-    Command();
-    Command(std::string type, void (*funct)());
+	private:
+		std::string	_type;
+		void		(*_funct)();
+	public:
+		Command();
+		Command(std::string type, void (*funct)());
 
-    void        setFunct(void (*_funct)());
-    void        setType(std::string type);
-    void        (*getFunct())();
-    std::string getType(void);
-    ~Command();
+		void		setFunct(void (*_funct)());
+		void		setType(std::string type);
+		void		(*getFunct())();
+		std::string	getType(void);
+		~Command();
 };
 
 #endif
